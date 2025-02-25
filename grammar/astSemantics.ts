a98ff58b0a01bd155390131c9cc5061b4e5cdf0f
sechmo <sesanchezmo@unal.edu.co>
2025-02-24 19:36:30 -0500

    grammar fixes and remaining ast nodes

--- grammar/astSemantics.ts
+++ grammar/astSemantics.ts
@@ -31,31 +31,31 @@ astSemantics.addOperation<SourceLocation>("getLoc", {
 });
 
 astSemantics.addOperation<boolean>("booleanValue", {
-    booleanLiteral(val) {
-        return val.booleanValue();
-    },
-    false(_FALSE) {
-        return false;
-    },
-    true(_TRUE) {
-        return true;
-    },
-})
+  booleanLiteral(val) {
+    return val.booleanValue();
+  },
+  false(_FALSE) {
+    return false;
+  },
+  true(_TRUE) {
+    return true;
+  },
+});
 
 astSemantics.addOperation<AbstractSymbol>("getSymbol", {
-    objectIdentifier(_lower, _rest) {
-        return AbstractTable.idTable.add(this.sourceString);
-    },
-    typeIdentifier(_upper, _rest) {
-        return AbstractTable.idTable.add(this.sourceString);
-    },
-    integerLiteral(_) {
-        return AbstractTable.idTable.add(this.sourceString);
-    },
+  objectIdentifier(_lower, _rest) {
+    return AbstractTable.idTable.add(this.sourceString);
+  },
+  typeIdentifier(_upper, _rest) {
+    return AbstractTable.idTable.add(this.sourceString);
+  },
+  integerLiteral(_) {
+    return AbstractTable.idTable.add(this.sourceString);
+  },
 
-    stringLiteral(_LQ, inside,_RQ) {
-        return AbstractTable.stringTable.add(inside.sourceString)
-    },
+  stringLiteral(_LQ, inside, _RQ) {
+    return AbstractTable.stringTable.add(inside.sourceString);
+  },
 });
 
 astSemantics.addOperation<AST.ASTNode>("toAST", {
@@ -68,7 +68,7 @@ astSemantics.addOperation<AST.ASTNode>("toAST", {
     );
   },
 
-  ClassStatement(
+  ClassStatement_WithParent(
     _CLASS,
     className,
     _INHERITS,
@@ -81,23 +81,40 @@ astSemantics.addOperation<AST.ASTNode>("toAST", {
     return new AST.ClassStatement(
       this.getLoc(),
       className.getSymbol(),
-      parentName.sourceString === "" ? null : parentName.toAST(),
+      parentName.getSymbol(),
       features.children.map((f) => f.toAST()),
     );
   },
 
+  ClassStatement_WithoutParent(
+    _CLASS,
+    className,
+    _LB,
+    features,
+    _SEMIS,
+    _RB,
+  ) {
+    return new AST.ClassStatement(
+      this.getLoc(),
+      className.getSymbol(),
+      null,
+      features.children.map((f) => f.toAST()),
+    );
+  },
   Feature(feat) {
     return feat.toAST();
   },
 
   Attribute(name, _COLOR, tyepDecl, _ARROW, init) {
-      const actualInit = init.sourceString === "" ? new AST.NoExpr(this.getLoc()) : init.toAst();
-      return new AST.Attribute(
-          this.getLoc(),
-          name.getSymbol(),
-          tyepDecl.getSymbol(),
-          actualInit
-      );
+    const actualInit = init.sourceString === ""
+      ? new AST.NoExpr(this.getLoc())
+      : init.toAst();
+    return new AST.Attribute(
+      this.getLoc(),
+      name.getSymbol(),
+      tyepDecl.getSymbol(),
+      actualInit,
+    );
   },
 
   Method(methName, _LP, formals, _RP, _COLON, returnType, _LB, bodyExpr, _RB) {
@@ -111,14 +128,18 @@ astSemantics.addOperation<AST.ASTNode>("toAST", {
   },
 
   Formal(name, _COLON, typeDecl) {
-      return new AST.Formal(this.getLoc(), name.getSymbol(), typeDecl.getSymbol());
+    return new AST.Formal(
+      this.getLoc(),
+      name.getSymbol(),
+      typeDecl.getSymbol(),
+    );
   },
 
   Expr(expr) {
     return expr.toAST();
   },
 
-  Expr_LessThan(lhs, _LT, rhs) {
+  ComparisonExpr_LessThan(lhs, _LT, rhs) {
     return new AST.LessThan(
       this.getLoc(),
       lhs.toAST(),
@@ -126,7 +147,7 @@ astSemantics.addOperation<AST.ASTNode>("toAST", {
     );
   },
 
-  Expr_LessThanOrEqual(lhs, _LT, rhs) {
+  ComparisonExpr_LessThanOrEqual(lhs, _LT, rhs) {
     return new AST.LessThanOrEqual(
       this.getLoc(),
       lhs.toAST(),
@@ -134,7 +155,7 @@ astSemantics.addOperation<AST.ASTNode>("toAST", {
     );
   },
 
-  Expr_Equal(lhs, _LT, rhs) {
+  ComparisonExpr_Equal(lhs, _LT, rhs) {
     return new AST.Equal(
       this.getLoc(),
       lhs.toAST(),
@@ -183,25 +204,27 @@ astSemantics.addOperation<AST.ASTNode>("toAST", {
   },
 
   DispatchExpression(expr) {
-    console.log("here", this.ctorName, this.sourceString);
     return expr.toAST();
   },
 
   DispatchExpression_Dynamic(caller, _DOT, methName, _LP, args, _RP) {
-    const actualCaller = caller.sourceString === ""
-      ? new AST.ObjectReference(
-        this.getLoc(),
-        AbstractTable.idTable.add("self"),
-      )
-      : caller.toAST();
-
-      console.log("here", this.ctorName);
-    
     return new AST.DynamicDispatch(
       this.getLoc(),
-      actualCaller,
+      caller.toAST(),
       methName.getSymbol(),
-      args.children.map((e) => e.toAST()),
+      args.asIteration().children.map((e) => e.toAST()),
+    );
+  },
+
+  DispatchExpression_DynamicNoCaller(methName, _LP, args, _RP) {
+    return new AST.DynamicDispatch(
+      this.getLoc(),
+      new AST.ObjectReference(
+        this.getLoc(),
+        AbstractTable.idTable.add("self"),
+      ),
+      methName.getSymbol(),
+      args.asIteration().children.map((e) => e.toAST()),
     );
   },
 
@@ -232,61 +255,106 @@ astSemantics.addOperation<AST.ASTNode>("toAST", {
     return expr.toAST();
   },
 
+  IfExpression(_IF, pred, _THEN, thenExpr, _ELSE, elseExpr, _FI) {
+    return new AST.Conditional(
+      this.getLoc(),
+      pred.toAST(),
+      thenExpr.toAST(),
+      elseExpr.toAST(),
+    );
+  },
+
+  CaseExpression(_CASE, expr, _OF, branches, _SEMIS, _ESAC) {
+    return new AST.TypeCase(
+      this.getLoc(),
+      expr.toAST(),
+      branches.children.map((c) => c.toAST()),
+    );
+  },
+
+  Branch(name, _COLON, typeCheck, _RARROW, body) {
+    return new AST.Branch(
+      this.getLoc(),
+      name.getSymbol(),
+      typeCheck.getSymbol(),
+      body.toAST(),
+    );
+  },
+
   LetExpression(_LET, letAux) {
-      return letAux.toAST();
+    return letAux.toAST();
   },
 
   LetAux_finalWithInit(name, _COLON, typeDecl, _ARROW, init, _IN, body) {
-      return new AST.Let(
-          this.getLoc(),
-          name.getSymbol(),
-          typeDecl.getSymbol(),
-          init.toAST(),
-          body.toAST(),
-      );
+    return new AST.Let(
+      this.getLoc(),
+      name.getSymbol(),
+      typeDecl.getSymbol(),
+      init.toAST(),
+      body.toAST(),
+    );
   },
 
   LetAux_finalWithoutInit(name, _COLON, typeDecl, _IN, body) {
-      return new AST.Let(
-          this.getLoc(),
-          name.getSymbol(),
-          typeDecl.getSymbol(),
-          new AST.NoExpr(this.getLoc()),
-          body.toAST(),
-      );
+    return new AST.Let(
+      this.getLoc(),
+      name.getSymbol(),
+      typeDecl.getSymbol(),
+      new AST.NoExpr(this.getLoc()),
+      body.toAST(),
+    );
   },
 
   LetAux_midWithInit(name, _COLON, typeDecl, _ARROW, init, _COMMA, otherLet) {
-      return new AST.Let(
-          this.getLoc(),
-          name.getSymbol(),
-          typeDecl.getSymbol(),
-          init.toAST(),
-          otherLet.toAST(),
-      );
+    return new AST.Let(
+      this.getLoc(),
+      name.getSymbol(),
+      typeDecl.getSymbol(),
+      init.toAST(),
+      otherLet.toAST(),
+    );
   },
 
   LetAux_midWithoutInit(name, _COLON, typeDecl, _COMMA, otherLet) {
-      return new AST.Let(
-          this.getLoc(),
-          name.getSymbol(),
-          typeDecl.getSymbol(),
-          new AST.NoExpr(this.getLoc()),
-          otherLet.toAST(),
-      );
+    return new AST.Let(
+      this.getLoc(),
+      name.getSymbol(),
+      typeDecl.getSymbol(),
+      new AST.NoExpr(this.getLoc()),
+      otherLet.toAST(),
+    );
   },
-    
+
+  AssigmentExpression(name, _ARROW, expr) {
+    return new AST.Assignment(
+      this.getLoc(),
+      name.getSymbol(),
+      expr.toAST(),
+    );
+  },
 
   WhileExpression(_WHILE, pred, _LOOP, body, _POOL) {
-      return new AST.Loop(
-          this.getLoc(),
-          pred.toAST(),
-          body.toAST()
-      );
+    return new AST.Loop(
+      this.getLoc(),
+      pred.toAST(),
+      body.toAST(),
+    );
+  },
+
+  NewExpression(_NEW, typeName) {
+    return new AST.New(this.getLoc(), typeName.getSymbol());
+  },
+
+  Negation(_NEG, expr) {
+    return new AST.Negation(this.getLoc(), expr.toAST());
   },
 
   NotExpression(_NOT, expr) {
-      return new AST.Complement(this.getLoc(), expr.toAST());
+    return new AST.Complement(this.getLoc(), expr.toAST());
+  },
+
+  IsVoidExpression(_ISVOID, expr) {
+    return new AST.IsVoid(this.getLoc(), expr.toAST());
   },
 
   BlockExpression(_LB, exprs, _SEMIS, _RB) {
@@ -303,27 +371,25 @@ astSemantics.addOperation<AST.ASTNode>("toAST", {
     );
   },
 
-
   IntegerConst(val) {
     return new AST.IntegerConstant(
       this.getLoc(),
       val.getSymbol(),
     );
-    },
-  
+  },
 
   BooleanConst(val) {
-      return new AST.BooleanConstant(
-          this.getLoc(),
-          val.booleanValue(),
-      );
+    return new AST.BooleanConstant(
+      this.getLoc(),
+      val.booleanValue(),
+    );
   },
 
   StringConst(val) {
-      return new AST.StringConstant(
-          this.getLoc(),
-          val.getSymbol(),
-      );
+    return new AST.StringConstant(
+      this.getLoc(),
+      val.getSymbol(),
+    );
   },
 });
 
